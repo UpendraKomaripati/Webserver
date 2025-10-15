5cf6cfea31ba9744552aca3b0992ac7f7b186a26
Upendra Komaripati <upendrakomaripaticse@gmail.com>
2025-10-15 11:32:40 +0530

    Add routes

--- App.js
+++ App.js
@@ -5,6 +5,15 @@ app.get("/apple",(req,res)=>{
     res.send("Apple is red color")
 
 })
+app.get("/banana",(req,res)=>{
+    res.send('Banana is a yellow color')
+})
+app.get("/guava",(req,res)=>{
+    res.send("Guava is a green color")
+})
+app.get("/orange",(req,res)=>{
+    res.send("Orange is a orange color")
+})
 app.listen(4455,()=>{
     console.log("Server is started")
 })
